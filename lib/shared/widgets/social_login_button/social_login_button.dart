20fb08a676e1361443e3d01ae98cf0c12abb9384
Felipe Sales <[soufeliposales@gmail.com]>
2021-06-21 23:48:37 -0300

    Adding the SocialLoginButton

---
+++ lib/shared/widgets/social_login_button/social_login_button.dart
@@ -0,0 +1,52 @@
+import 'package:flutter/material.dart';
+
+import '/../shared/themes/app_colors.dart';
+import '/../shared/themes/app_images.dart';
+import '/../shared/themes/app_text_styles.dart';
+
+class SocialLoginButton extends StatelessWidget {
+  final VoidCallback onTap;
+  const SocialLoginButton({Key? key, required this.onTap}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return InkWell(
+      onTap: onTap,
+      child: Container(
+        height: 56,
+        decoration: BoxDecoration(
+          color: AppColors.shape,
+          borderRadius: BorderRadius.circular(5),
+          border: Border.fromBorderSide(BorderSide(color: AppColors.stroke)),
+        ),
+        child: Row(
+          children: [
+            Expanded(
+              flex: 1,
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Image.asset(AppImages.google),
+                  SizedBox(width: 12),
+                  Container(height: 56, width: 1, color: AppColors.stroke)
+                ],
+              ),
+            ),
+            Expanded(
+              flex: 4,
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text(
+                    'Enter with Google',
+                    style: AppTextStyles.buttonGrey,
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
