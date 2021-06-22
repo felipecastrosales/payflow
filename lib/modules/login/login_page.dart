d101b472a8069d28c55ba0b49c6847a24017aa04
Felipe Sales <[soufeliposales@gmail.com]>
2021-06-21 23:45:25 -0300

    Adding the LoginPage

---
+++ lib/modules/login/login_page.dart
@@ -0,0 +1,73 @@
+import 'package:flutter/material.dart';
+
+import '../../shared/themes/app_colors.dart';
+import '../../shared/themes/app_images.dart';
+import '../../shared/themes/app_text_styles.dart';
+import '../../shared/widgets/social_login_button/social_login_button.dart';
+
+class LoginPage extends StatefulWidget {
+  const LoginPage({Key? key}) : super(key: key);
+  @override
+  _LoginPageState createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  @override
+  Widget build(BuildContext context) {
+    final size = MediaQuery.of(context).size;
+    return Scaffold(
+      backgroundColor: AppColors.background,
+      body: Container(
+        width: size.width,
+        height: size.height,
+        child: Stack(
+          children: [
+            Container(
+              width: size.width,
+              height: size.height * 0.36,
+              color: AppColors.primary,
+            ),
+            Positioned(
+              top: 40,
+              left: 0,
+              right: 0,
+              child: Image.asset(
+                AppImages.person,
+                width: size.width * 0.5,
+                height: size.height * 0.5,
+              ),
+            ),
+            Positioned(
+              bottom: size.height * 0.05,
+              left: 0,
+              right: 0,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Image.asset(AppImages.logomini),
+                  Padding(
+                    padding: const EdgeInsets.fromLTRB(70, 30, 70, 0),
+                    child: Text(
+                      'Organize your tickets in one place',
+                      textAlign: TextAlign.center,
+                      style: AppTextStyles.titleHome,
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.fromLTRB(40, 40, 40, 0),
+                    child: SocialLoginButton(
+                      onTap: () {
+                        debugPrint('tap!');
+                      },
+                    ),
+                  )
+                ],
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
