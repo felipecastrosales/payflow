2faaacebbfb04a95806d415cb1d03ffb49afb903
Felipe Sales <[soufeliposales@gmail.com]>
2021-06-23 01:03:17 -0300

    Updating the SplashPage with AuthController

--- lib/modules/splash/splash_page.dart
+++ lib/modules/splash/splash_page.dart
@@ -1,14 +1,17 @@
 import 'package:flutter/material.dart';
 
+import 'package:payflow/shared/auth/auth_controller.dart';
+
 import '../../shared/themes/app_images.dart';
 import '../../shared/themes/app_colors.dart';
-
 
 class SplashPage extends StatelessWidget {
   const SplashPage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
+    final authController = AuthController();
+    authController.currentUser(context);
     return Scaffold(
       backgroundColor: AppColors.background,
       body: Stack(
