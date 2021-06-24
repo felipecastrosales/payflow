5b9e539c7cbd48920b701094413ca90a17254910
Felipe Sales <[soufeliposales@gmail.com]>
2021-06-24 16:55:50 -0300

    Adding InsertBoletoPage

---
+++ lib/modules/insert_boleto/insert_boleto_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class InsertBoletoPage extends StatelessWidget {
+  const InsertBoletoPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Insert Boleto Page'),
+        centerTitle: true,
+      ),
+    );
+  }
+}
